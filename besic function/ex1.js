2d319d1bdd124e5abdb3ae1a6cea18a94ef292a5
vivek prajapati <vivekprajapati2536@gmail.com>
2023-09-01 17:55:41 +0530

    java

---
+++ besic function/ex1.js
@@ -0,0 +1,34 @@
+//1. No argu No return
+
+// function hello()
+// {
+//     console.log("hello world");
+// }
+// hello();
+
+//ex1
+// function add(a,b)
+// {
+// 	console.log(a+b);
+// }
+// add(5,15);
+
+//ex2
+// function add(a=3)
+// {
+// 	console.log("square:%d\n",a*a);
+// 	console.log("cube:%d\n",a*a*a);
+// }
+// add();
+
+//ex3
+function add(r=4000,dollar)
+{
+	console.log("dollar:%f\n",r/80);
+}
+add();
+
+
+
+
+
